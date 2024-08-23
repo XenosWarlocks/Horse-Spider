04b82f32b50903082faa19861356912d4555ca8c
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-23 14:36:28 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from backend.google_search import get_google_search_links
 from backend.scraper import run_scraper
 from backend.data_processing import convert_csv_to_excel, convert_csv_to_json
 from backend.database import store_data_to_db
-from database.db_config import create_db
+from database.db_config import DatabaseConfig
 
 def main():
     query = "ESG risks in bank investment portfolio"
@@ -16,8 +16,8 @@ def main():
     convert_csv_to_json("output/output.csv", "output/output.json")
 
     # Store data in the database
-    create_db("database/esg_data.db")
-    store_data_to_db("output/output.csv", "database/esg_data.db")
+    db_config = DatabaseConfig("database/esg_data.db")
+    store_data_to_db("output/output.csv", db_config)
 
 if __name__ == "__main__":
     main()
